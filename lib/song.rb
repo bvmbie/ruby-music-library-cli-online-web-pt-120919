59b3093bfed730eb3749c5926276c52828ae3102
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-04 10:46:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -71,6 +71,7 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
+    self.name 
   end
 
 end
